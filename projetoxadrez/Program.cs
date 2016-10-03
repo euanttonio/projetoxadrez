d9ba255e00dccf8d6f67492b86abdbc81266bbf7
ANTONIO TEIXEIRA JUNQUEIRA NETO <01867905620@est.udicentro.local>
2016-10-03 08:45:51 -0300

    comecando a implementar o metodo que imprime o tabuleiro

--- projetoxadrez/Program.cs
+++ projetoxadrez/Program.cs
@@ -9,6 +9,8 @@ namespace projetoxadrez
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
              
         }
